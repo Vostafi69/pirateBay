b5f90c34e897b90071cbdf6827c7329159725eb7
Alexander <a.thorik@yandex.ru>
2024-10-04 23:28:48 +0300

    feat: Добавлена заготовка для фичи подключения кошелька

---
+++ src/features/connectWallet/ui/connectWallerButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui/button";
+import { FC } from "react";
+
+export const ConnectWalletButton: FC = () => {
+  return (
+    <Button className="h-[63px] rounded-[10px] border-none px-6 py-4 text-[26px] tracking-tight text-[#FEEFCD]">
+      Connect wallet
+    </Button>
+  );
+};
