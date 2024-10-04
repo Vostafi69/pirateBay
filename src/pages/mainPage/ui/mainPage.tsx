16201f7290a59c14ad860cfe69c0fafbfeaf9af8
Alexander <a.thorik@yandex.ru>
2024-10-04 23:27:35 +0300

    fix: Фон перенесен в layout

--- src/pages/mainPage/ui/mainPage.tsx
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -1,7 +1,5 @@
 import { FC } from "react";
 
 export const MainPage: FC = () => {
-  return (
-    <section className="h-full w-full bg-[url('/public/pirateBack.png')] bg-cover bg-bottom bg-no-repeat"></section>
-  );
+  return <section className="h-full w-full"></section>;
 };
