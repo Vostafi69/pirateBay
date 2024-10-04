b4760fdd63c0ee780f1e0ad6e34913f9cbe925d8
Alexander <a.thorik@yandex.ru>
2024-10-04 23:26:27 +0300

    feat: Добавлены компоненты button и logo

---
+++ src/shared/ui/logo.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+
+export const Logo: FC = () => {
+  return (
+    <div className="flex h-[96px] w-[268px] flex-row items-center justify-between overflow-hidden rounded-[15px] border-4 border-border bg-[url('/logoBack.png')] bg-cover bg-center p-4">
+      <a className="relative block h-16 w-16" href="#">
+        <img
+          className="absolute w-full object-cover"
+          src="/logo.svg"
+          alt="PirateBay"
+        />
+      </a>
+      <p className="font-PirateOne select-none text-[45px] tracking-tight text-[#352831]">
+        PirateBay
+      </p>
+    </div>
+  );
+};
