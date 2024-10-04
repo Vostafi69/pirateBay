7fc1690a53056413a40d3e55db51d303bb40fa4a
Alexander <a.thorik@yandex.ru>
2024-10-04 23:48:29 +0300

    fix: Исправлен бэкгроунд layout'a

--- src/app/layouts/mainLayout.tsx
+++ src/app/layouts/mainLayout.tsx
@@ -11,7 +11,7 @@ export const MainLayout: FC<MainLayoutProps> = ({
   headerSlot,
 }) => {
   return (
-    <div className="font-TTFirsNeu flex h-screen w-full max-w-[1920px] flex-col bg-[url('/pirateBack.png')] bg-cover bg-bottom bg-no-repeat px-10 py-8">
+    <div className="font-TTFirsNeu relative flex h-screen w-full max-w-[1920px] flex-col px-10 py-8 after:absolute after:inset-0 after:-z-[1] after:bg-[url('/pirateBack.png')] after:bg-cover after:bg-bottom after:bg-no-repeat after:opacity-60">
       {headerSlot}
       <main className="h-full w-full">{children}</main>
       {footerSlot}
